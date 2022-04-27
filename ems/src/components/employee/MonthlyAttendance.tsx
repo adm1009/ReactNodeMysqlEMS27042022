fac98f1406795eea8b2bcf8f65eeeb048214a157
Abhijeet <abhijeetmulik319@gmail.com>
2022-04-27 19:25:44 +0530

    first commit

---
+++ ems/src/components/employee/MonthlyAttendance.tsx
@@ -0,0 +1,95 @@
+// import React from 'react';
+
+// import { Calendar } from 'react-big-calendar';
+
+// import moment from 'moment';
+
+// class Calendar extends React.Component {
+
+//    render() {
+
+//        return (
+
+//            <Calendar
+
+//                startAccessor="start"
+
+//                endAccessor="end"
+
+//            />
+
+//        )
+
+//    }
+// }
+// export default Calendar;
+// import React, { Component } from "react";
+// import Calendar from "react-calendar";
+// import Navbar from "../Navbar";
+// import { Navigate} from "react-router-dom";
+// import "../Home.css";
+// type props = {
+//   date:any
+//   username:any
+// }
+
+// class MonthlyAttendance extends React.Component<props,props> {
+//   constructor(props: props) {
+//     super(props);
+//     this.state = {
+//       date: new Date(),
+//       username:this.props.username
+//     };
+//   }
+
+//   onChange = (date: any) => this.setState({ date });
+
+//   render() {
+   
+//     return (
+//       <>
+//         <Navbar personalData leaveData employeeData myaccount username={this.props.username}/>
+//         <div style={{textAlign:"center"}}>
+//         <span style={{ textDecoration: "underline" }}>Monthly Attendance</span>
+//         <hr />
+//         <Calendar onChange={this.onChange} value={this.state.date} />
+//       </div>
+//       </>
+//     );
+//   }
+// }
+// export default MonthlyAttendance;
+import React from 'react';
+import Navbar from '../Navbar';
+class MonthlyAttendance extends React.Component<any,any> {
+  render(){
+  return (<>
+    <Navbar  personalData
+    leaveData
+    myaccount
+    employeeData
+    username={this.props.username}/>
+    <div style={{textAlign:"center"}}>MonthlyAttendance</div>
+    <hr />
+    </>
+  )
+}
+}
+export default MonthlyAttendance;
+// import React from 'react';
+// import Navbar from '../Navbar';
+// const MonthlyAttendance = () => {
+//   return (<>
+//     <Navbar  personalData
+//     leaveData
+//     myaccount
+//     employeeData
+//     username={this.props.username}/>
+//     <div style={{textAlign:"center"}}>MonthlyAttendance</div>
+//     <hr />
+//     </>
+//   )
+// }
+
+// export default MonthlyAttendance
+
